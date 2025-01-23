8062871f7ea200b81a3ec1b2b902b07baec7a10c
OTMAN HERRERA <otman.herrera@triarccorp.com>
2025-01-23 10:40:46 -0600

    2025-01-23

--- ejemplo_tidyr.R
+++ ejemplo_tidyr.R
@@ -68,3 +68,79 @@ df
 df %>% pivot_longer(-A, 
                     names_to = c('B', '.value'), 
                     names_sep = '_')
+
+# expand_grid
+expand_grid(c_1 = 0:3, c_2 = 0:3)
+
+# right_join(df, by = c(x, y))
+
+# anti_join
+
+# complete
+df <- data.frame(c1 = c(1, 3, 3), 
+                 c2 = c('A', 'A', 'B'), 
+                 c3 = c(1, 2, 3))
+df
+df %>% complete(c1, c2)
+df %>% complete(c1, c2, fill = list(c3 = 0L))
+df %>% 
+  complete(c1 = full_seq(df[['c1']], period = 1), c2, fill = list(c3 = 0L))
+
+# unnest_wider unnest_longer
+x <- list(list(c_1 = 'A', c_2 = 1, c_list = list(a = 1, b = 2, c = 3)), 
+          list(c_1 = 'B', c_2 = 2, c_list = list(a = 15, b = 20)))
+x
+
+tibble(col = x) %>% unnest_wider(col) %>% unnest_longer(c_list)
+
+# hoist
+tibble(col = x) %>%
+  hoist(col, 'col_A' = list('c_1'))
+
+tibble(col = x) %>%
+  hoist(col, 'col_B' = list('c_2'))
+
+tibble(col = x) %>%
+  hoist(col, 'col_A' = list('c_1'), 'a' = list('c_list', 'a'), 'b' = list('c_list', 'b'))
+
+tibble(col = x) %>%
+  hoist(col, 'col_A' = 'c_1', 'a' = list('c_list', 'a'), 'b' = list('c_list', 'b'))
+
+str(x)
+
+# glance, tidy, 
+library(dplyr)
+library(broom)
+library(purrr)
+df <- data.frame(x = c(1, 2, 3, 1, 2, 3), 
+                 y = c(1.1, 1.9, 3.2, 0.9, 1.9, 3.3), 
+                 z = c('a', 'a', 'a', 'b', 'b', 'b'))
+df
+
+model <- lm(y ~ x, data = df)
+model
+summary(model)
+
+glance(model)
+tidy(model)
+
+map(list(l.1 = c(1,1,1), l.2 = c(1,2,3)), sum)
+
+map(df[c('x','y')], sum)
+
+df %>%
+  group_by(z) %>%
+  nest() %>%
+  mutate(fit = map(data, function(x){lm(y ~ x, data = x)}), 
+         glance = map(fit, glance), 
+         tidy = map(fit, tidy)) %>%
+  unnest(glance)
+
+df_2 = df %>% 
+  group_by(z) %>%
+  nest() %>%
+  mutate(fit = map(data, function(x){lm(y ~ x, data = x)}), 
+         glance = map(fit, glance), 
+         tidy = map(fit, tidy))
+df_2 %>% unnest(glance)
+df_2 %>% unnest(tidy)
